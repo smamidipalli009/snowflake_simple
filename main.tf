5c2161cd3dfef5c21b9b01bc506a3d02fffe61d4
smamidipalli009 <mamidipalli009@gmail.com>
2026-07-21 08:45:40 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ provider "snowflake" {
 module "learning_db" {
   source = "git::https://github.com/smamidipalli009/snowflake-terraform.git//modules/database?ref=v1.0.0"
 
-  name = "LEARNING_DB"
+  name = "SWETA_DB"
 }
 
 output "database_name" {
